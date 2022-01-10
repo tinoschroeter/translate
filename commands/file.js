4f4966af426045eb9534be46b2c0b690d4e0a615
tinoschroeter <mail@wgnetworks.de>
2022-01-11 00:39:51 +0100

    fix wrong variable

--- commands/file.js
+++ commands/file.js
@@ -7,7 +7,7 @@ translate.key = process.env.DEEPL_KEY;
 
 let lang;
 
-const file = (file, setLang) => {
+const file = (filename, setLang) => {
   if (!setLang) lang = { from: "de", to: "en" };
   if (setLang && setLang.length < 2) return langError();
   if (setLang) lang = { from: setLang[0], to: setLang[1] };
